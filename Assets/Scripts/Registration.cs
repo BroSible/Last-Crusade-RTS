a28c769e27efafe6f594a6bff59330723ff7aed0
ReunionRS <167036911+ReunionRS@users.noreply.github.com>
2025-01-13 15:23:21 +0400

    menu and rega

--- Assets/Scripts/Registration.cs
+++ Assets/Scripts/Registration.cs
@@ -5,7 +5,6 @@ using Mono.Data.Sqlite;
 using UnityEngine.Analytics;
 using System;
 using UnityEngine.SceneManagement;
-using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 public class Registration : MonoBehaviour
@@ -22,14 +21,16 @@ public class Registration : MonoBehaviour
     {
         CreateDB();
     }
+
     private bool ValidatePassword(string password)
     {
-        Regex regex = new Regex("^(?=.*[A-Z])(?=.*\\d)(?!.*\\s).{8,}$");
+        Regex regex = new Regex("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-z]).{8,}$");
         return regex.IsMatch(password);
     }
+
     private bool ValidateUsername(string username)
     {
-        Regex regex = new Regex("^(?=.*[A-Z])[a-zA-Z]{3}[a-z]*$");
+        Regex regex = new Regex("^[A-Z][a-z]{2,}$");
         return regex.IsMatch(username);
     }
 
@@ -38,51 +39,56 @@ public class Registration : MonoBehaviour
         using (var connection = new SqliteConnection(dbName))
         {
             connection.Open();
-
             using (var command = connection.CreateCommand())
             {
                 command.CommandText = "CREATE TABLE IF NOT EXISTS users (username VARCHAR(20), password VARCHAR(20));";
                 command.ExecuteNonQuery();
             }
-
             connection.Close();
         }
     }
-   public void ConfirmPas()
+
+    public void ConfirmPas()
     {
         string username = usernameField.text;
         string password = passwordField.text;
         string doublePas = doublePas1.text;
-        if (doublePas == "" || doublePas == null)
+        if (string.IsNullOrEmpty(doublePas))
         {
-            doublePas1.gameObject.SetActive(true);
             errorText.text = "CONFRIM YOUR PASSWORD";
         }
         else if (doublePas == password)
         {
-            errorText.text = "You are registed";
             Register();
         }
-        else if(doublePas != password)
+        else
         {
             errorText.text = "You are input the wrong password";
         }
-
     }
 
     public void Login()
     {
         string username = usernameField.text;
         string password = passwordField.text;
-        if (username != "" && password != "")
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
         {
-            using (var connection = new SqliteConnection(dbName))
+            errorText.text = "INPUT SOME";
+            return;
+        }
+
+        using (var connection = new SqliteConnection(dbName))
+        {
+            try
             {
                 connection.Open();
-
                 using (var command = connection.CreateCommand())
                 {
-                    command.CommandText = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "';";
+                   command.CommandText = "SELECT * FROM users WHERE username = @username AND password = @password";
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
                     using (IDataReader reader = command.ExecuteReader())
                     {
                         if (reader.Read())
@@ -90,74 +96,98 @@ public class Registration : MonoBehaviour
                             errorText.text = "Your welcome";
                             Debug.Log("Login successful");
                             Enter = true;
-                            SceneManager.LoadScene(1);
-
+                            SceneManager.LoadScene("Menu");
                         }
                         else
                         {
                             errorText.text = "Invalid username or password";
                             Debug.Log("Login failed");
                         }
-                        reader.Close();
+                         reader.Close();
                     }
                 }
-
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Error during login: {ex.Message}");
+                errorText.text = "Login failed. Please try again later.";
+            }
+            finally
+            {
                 connection.Close();
             }
         }
-        else
-        {
-            errorText.text = "INPUT SOME";
-        }
     }
 
-    public void Register()
+
+ public void Register()
     {
         string username = usernameField.text;
         string password = passwordField.text;
         string confirmPassword = doublePas1.text;
 
-        if (username != "" && password != "" && confirmPassword != "")
-        {
-            if (ValidateUsername(username))
-            {
-                if (password == confirmPassword && ValidatePassword(password))
-                {
-                    using (var connection = new SqliteConnection(dbName))
-                    {
-                        connection.Open();
-                        using (var command = connection.CreateCommand())
-                        {
-                            command.CommandText = "SELECT COUNT(*) FROM users WHERE username='" + username + "';";
-                            int count = Convert.ToInt32(command.ExecuteScalar());
-                            if (count > 0)
-                            {
-                                errorText.text = "User already exists";
-                                Debug.Log("User already exists");
-                                return;
-                            }
-                            command.CommandText = "INSERT INTO users (username, password) VALUES ('" + username + "', '" + password + "');";
-                            command.ExecuteNonQuery();
-                        }
-                        connection.Close();
-                    }
-                    Enter = true;
-                    SceneManager.LoadScene(1);
-                }
-                else
-                {
-                    errorText.text = "Passwords do not match or do not meet the requirements";
-                }
-            }
-            else
-            {
-                errorText.text = "Username must be at least 8 letters and contain only English alphabet letters";
-            }
-        }
-        else
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
         {
             errorText.text = "Please fill in all fields";
+            return;
+        }
+
+        if (!ValidateUsername(username))
+        {
+            errorText.text = "Invalid username format. Must start with a capital letter, followed by 2 or more lowercase letters.";
+            return;
+        }
+
+        if (!ValidatePassword(password))
+        {
+            errorText.text = "Invalid password format. Must contain at least 8 characters, one uppercase letter, one digit, and at least one lowercase letter.";
+            return;
+        }
+
+
+        if (password != confirmPassword)
+        {
+            errorText.text = "Passwords do not match";
+            return;
+        }
+
+        using (var connection = new SqliteConnection(dbName))
+        {
+             try
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT COUNT(*) FROM users WHERE username = @username";
+                    command.Parameters.AddWithValue("@username", username);
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        errorText.text = "User already exists";
+                        Debug.Log("User already exists");
+                         return;
+                    }
+                   
+                  command.CommandText = "INSERT INTO users (username, password) VALUES (@username, @password)";
+                    command.Parameters.AddWithValue("@username", username);
+                     command.Parameters.AddWithValue("@password", password);
+                    command.ExecuteNonQuery();
+                 
+                    errorText.text = "Registration successful!";
+                   SceneManager.LoadScene("Menu");
+                }
+            }
+             catch(Exception ex)
+            {
+                Debug.LogError($"Error during registration: {ex.Message}");
+                errorText.text = "Registration failed. Please try again later.";
+            }
+              finally
+            {
+                connection.Close();
+            }
         }
     }
-
 }
